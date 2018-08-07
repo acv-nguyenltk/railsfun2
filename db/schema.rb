8719fc95e3d2d351b6f28d37a469f3445f891f85
Nguyen <nguyenltk@atmarkcafe.org>
2018-08-07 10:24:07 +0700

    Add reference to categories in products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_07_030350) do
+ActiveRecord::Schema.define(version: 2018_08_07_030934) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "products", force: :cascade do |t|
     t.string "title"
