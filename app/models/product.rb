dd0a14bacee9f2d5373ddba93a8400bd2ea86337
Nguyen <nguyenltk@atmarkcafe.org>
2018-08-07 17:12:50 +0700

    Add controller and index view, show data

--- app/models/product.rb
+++ app/models/product.rb
@@ -13,6 +13,7 @@ class Product < ActiveRecord::Base
   enumerize :level, in: {easy: 1, medium: 2, hard: 3}
   enumerize :country, in: ISO3166::Country.translations.invert
 
+
   def set_title_lowercase
     self.title = title.downcase
   end
