26bf5f613902e9ae0fb4191a4510737366033178
Nguyen <nguyenltk@atmarkcafe.org>
2018-08-08 16:05:44 +0700

    Add function delete

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -38,6 +38,17 @@ class ProductsController < ApplicationController
     end
   end
 
+  def destroy
+    @product = Product.find(params[:id])
+    if @product.destroy
+      flash[:notice] = "Deleted"
+      redirect_to products_url
+    else
+      flash[:notice] = "Not yet"
+      redirect_to products_url
+    end
+  end
+
   private def product_params
     params.require(:product).permit( :title,
     :desciption, :level, :category_id, :price, :country, :published)
